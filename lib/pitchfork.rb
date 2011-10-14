75165571afb67cb60809052f5f384d32167ec9a2
Peter Bui <peter@paydrotalks.com>
2011-10-14 16:01:04 -0700

    Add signals to pitchfork
    Pitchfork can now take the following signals:
    
    * TERM/INT - to shutdown immediately
    * QUIT - gracefully shutdown
    * STOP - to pause processing
    * CONT - to continue processing

--- lib/pitchfork.rb
+++ lib/pitchfork.rb
@@ -1,8 +1,10 @@
 require "pitchfork/version"
 
 module Pitchfork
+  class PitchforkError < ::StandardError; end
+  class MissingBlock < PitchforkError; end
 
-  def pitch(collection, options = {}, &block)
+  def self.pitch(collection, options = {}, &block)
     Handler.new(collection, options).pitch(&block)
   end
 
@@ -11,29 +13,105 @@ module Pitchfork
 
     def initialize(collection, config = {})
       @collection = collection
-      @config = config
-      @config[:forks] ||= 2 # At most 2 children
-      @children = 0
+      @config = {:forks => 2, :name => "pitchfork"}.merge(config)
+      @children = {}
+      @status = :work
+      @master_pid = Process.pid
     end
 
     def pitch(&block)
+      puts "Current pid is: #{@master_pid}"
+      register_signals
+
+      procline "Spawning workers ..."
       collection.each do |data|
+        break unless work?
+
         if @child = fork
-          @children += 1
+          @children[@child] = true
         else
-          block.call(data) # Do the work!
+          procline "worker"
+          yield data
           exit
         end
 
         # Stop forking and wait for a child if we've reached the limit
-        if @children >= @config[:forks]
-          Process.wait
-          @children -= 1
+        if @children.size >= @config[:forks]
+          procline "Waiting for workers to finish ..."
+          pid = Process.wait
+          @children.delete(pid)
+        end
+
+        if pause?
+          loop do
+            break unless pause?
+            sleep 3
+          end
         end
       end
 
       Process.waitall
       collection
+    end
+
+    def work?
+      @status == :work
+    end
+
+    def pause?
+      @status == :pause
+    end
+
+    def quit?
+      @status == :quit
+    end
+
+    def register_signals
+      trap('TERM')  { shutdown! }
+      trap('INT')   { shutdown! }
+      trap('QUIT')  { shutdown }
+      trap('STOP')  { pause! }
+      trap('CONT')  { restart! }
+    end
+
+    def shutdown
+      @status = :quit
+      procline "Shutting down ..."
+    end
+
+    def shutdown!
+      shutdown
+
+      @children.keys.each do |pid|
+        if system("ps -p #{pid}")
+          Process.kill("KILL", pid) rescue nil
+        end
+      end
+    end
+
+    def pause!
+      procline "Paused processing"
+      puts "changing procline"
+      @status = :pause
+    end
+
+    def restart!
+      @status = :work
+    end
+
+    def master?
+      Process.pid == @master_pid
+    end
+
+    def child?
+      !master?
+    end
+
+    def procline(msg)
+      line = "#{@config[:name]}: "
+      line << "[#{@children.size}] " if master?
+      line << msg
+      $0 = line
     end
   end
 end
